d358ec75d2857608068891c850f658138c400d6e
Mohammed Naji <m.naji1512@gmail.com>
2022-12-19 16:57:06 +0200

    lec6

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,60 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Route::get(url, action);
+// Route::post(url, action);
+// Route::put(url, action);
+// Route::patch(url, action);
+// Route::delete(url, action);
 
-Route::get('/', function () {
-    return view('welcome');
+// use , namespace
+
+// 5 pages => home, about, contact, team, services
+
+Route::post('/', function() {
+    return 'home page';
 });
 
-Route::get('/new', function () {
-    return view('welcome');
+Route::put('/', function() {
+    return 'home page';
 });
+
+Route::patch('/', function() {
+    return 'home page';
+});
+
+Route::delete('/', function() {
+    return 'home page';
+});
+
+Route::get('', function() {
+    return 'home page - get';
+});
+
+Route::get('/about', function() {
+    return 'about page';
+});
+
+Route::get('/contact', function() {
+    return 'contact page';
+});
+
+Route::get('/team', function() {
+    return 'team page';
+});
+
+Route::get('/services', function() {
+    return 'services page';
+});
+
+// // https://bakkah.com/sessions
+// https://bakkah.com/sessions/capm
+// https://bakkah.com/sessions/ecba/self-study
+
+Route::get('user/{name?}', function($name = 'online') {
+    return 'user ' . $name;
+});
+
+// Route::get('user', function() {
+//     return 'user';
+// });
