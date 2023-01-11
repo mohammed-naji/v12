e786378a661daf5ce5b44ee7ed59c36dcd23b430
Mohammed Naji <m.naji1512@gmail.com>
2023-01-11 16:48:01 +0200

    lec14

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Post;
+use App\Models\Tag;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,11 +18,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        // Post::factory(10000)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
+        // User::create([
+        //     'name' => 'Mohammed',
+        //     'email' => 'moh@gmail.com',
+        //     'password' => bcrypt(123)
         // ]);
+
+        Tag::factory(100)->create();
     }
 }
